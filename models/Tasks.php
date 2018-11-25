de3158cf3a16302639791489a034c02a57cf311a
OHaidukova <OHaidukova@rambler.ru>
2018-11-25 15:20:23 +0300

    new

---
+++ models/Tasks.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\NotSupportedException;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "tasks".
+ *
+ * @property int $id
+ * @property string $number
+ * @property string $name
+ * @property string $details
+ * @property int $id_developer
+ * @property string $date_create
+ * @property string $date_resolve
+ * @property int $id_status
+ * @property string $date_change
+ * @property string $img
+ *
+ * @property User $developer
+ */
+class Tasks extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tasks';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['number', 'name'], 'required'],
+            [['id_developer', 'id_status'], 'integer'],
+            [['date_create', 'date_resolve', 'date_change'], 'safe'],
+            [['number', 'name'], 'string', 'max' => 50],
+            [['details'], 'string', 'max' => 500],
+            [['img'], 'string', 'max' => 200],
+            [['number'], 'unique'],
+            [['id_developer'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['id_developer' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'number' => 'Number',
+            'name' => 'Name',
+            'details' => 'Details',
+            'id_developer' => 'Id Developer',
+            'date_create' => 'Date Create',
+            'date_resolve' => 'Date Resolve',
+            'id_status' => 'Id Status',
+            'date_change' => 'Date Change',
+            'img' => 'Img',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDeveloper()
+    {
+        return $this->hasOne(User::className(), ['id' => 'id_developer']);
+    }
+
+    public static function getTasksCurrentMonth()
+    {
+        return static::find()
+            ->andWhere(['>', 'date_create', new Expression('LAST_DAY(CURDATE()) + INTERVAL 1 DAY - INTERVAL 1 MONTH') ])
+            ->andWhere(['<', 'date_create', new Expression('DATE_ADD(LAST_DAY(CURDATE()), INTERVAL 1 DAY)') ])
+            ->all();
+
+    }
+}
---
+++ models/TasksSearch.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Tasks;
+
+/**
+ * TasksSearch represents the model behind the search form of `app\models\tables\Tasks`.
+ */
+class TasksSearch extends Tasks
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'id_developer', 'id_status'], 'integer'],
+            [['number', 'name', 'details', 'date_create', 'date_resolve'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Tasks::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => 2
+            ]
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'id_developer' => $this->id_developer,
+            'date_create' => $this->date_create,
+            'date_resolve' => $this->date_resolve,
+            'id_status' => $this->id_status,
+        ]);
+
+        $query->andFilterWhere(['like', 'number', $this->number])
+            ->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'details', $this->details]);
+
+        return $dataProvider;
+    }
+}
